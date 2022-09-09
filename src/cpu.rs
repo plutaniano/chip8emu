2dbb86cbd444c4fe561274e1ed314434ba136504
Lucas R <github@lucas.rs>
2022-09-08 21:19:26 -0300

    update

--- src/cpu.rs
+++ src/cpu.rs
@@ -27,4 +27,39 @@ impl CPU {
             ST: 0,
         }
     }
+
+    pub fn run_instruction(&mut self, nibbles: u16) {
+        let a = nibbles & 0xF000 >> 12 as u8;
+        let b = nibbles & 0x0F00 >> 08 as u8;
+        let c = nibbles & 0x00F0 >> 04 as u8;
+        let d = nibbles & 0x000F >> 00 as u8;
+
+        match (a, b, c, d) {
+            (0x0, 0x0, 0xE, 0x0) => self.run_00e0(),
+            (0x0, 0x0, 0xE, 0xE) => self.run_00ee(),
+            (0x0, _, _, _) => self.run_0nnn(a + b + c),
+            (0x1, _, _, _) => self.run_1nnn(a + b + c),
+            (0x2, _, _, _) => self.run_2nnn(a + b + c),
+            (0x3, _, _, _) => self.run_3xkk(b, c + d),
+            (0x4, _, _, _) => self.run_4xkk(b, c + d),
+            (0x5, _, _, 0x0) => self.run_5xk0(b, c),
+            (0x6, _, _, _) => self.run_6xkk(b, c + d),
+            (0x7, _, _, _) => self.run_7xkk(b, c + d),
+            (0x8, _, _, 0x0) => self.run_8xy0(b, c),
+            (0x8, _, _, 0x1) => self.run_8xy1(b, c),
+            (0x8, _, _, 0x2) => self.run_8xy2(b, c),
+            (0x8, _, _, 0x3) => self.run_8xy3(b, c),
+            (0x8, _, _, 0x4) => self.run_8xy4(b, c),
+            (0x8, _, _, 0x5) => self.run_8xy5(b, c),
+            (0x8, _, _, 0x6) => self.run_8xy6(b, c),
+            (0x8, _, _, 0x7) => self.run_8xy7(b, c),
+            (0x8, _, _, 0xE) => self.run_8xyE(b, c),
+            (0x9, _, _, 0x0) => self.run_9xy0(b, c),
+            (0xA, _, _, _) => self.run_Annn(b + c + d),
+            (0xB, _, _, _) => self.run_Bnnn(b + c + d),
+            (0xC, _, _, _) => self.run_Cnnn(b, c + d),
+            (0xD, _, _, _) => self.run_Dnnn(b, c, d),
+            (0xB, _, _, _) => self.run_Annn(b + c + d),
+        }
+    }
 }
