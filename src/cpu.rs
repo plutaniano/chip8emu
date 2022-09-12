10b3b3f2d5d3c6351d2d7105f8e9be292ccd2c79
Lucas R <github@lucas.rs>
2022-09-12 11:13:53 -0300

    work

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,26 +5,27 @@ const DISPLAY_WIDTH: usize = 32;
 
 struct CPU {
     ram: [u8; RAM_SIZE],
-    V: [u8; 16],
-    I: u16,
-    PC: u16,
-    SP: u16,
+    v: [u8; 16],
+    i: u16,
+    pc: u16,
+    sp: u16,
     stack: [u16; STACK_SIZE],
-    DT: u16,
-    ST: u16,
+    dt: u16,
+    st: u16,
 }
+
 
 impl CPU {
     pub fn new() -> Self {
         Processor {
             ram: [0; RAM_SIZE],
-            V: [u8; 0],
-            I: 0,
-            PC: 0x200,
-            SP: 0,
+            v: [u8; 0],
+            i: 0,
+            pc: 0x200,
+            sp: 0,
             stack: [0; STACK_SIZE],
-            DT: 0,
-            ST: 0,
+            dt: 0,
+            st: 0,
         }
     }
 
@@ -55,11 +56,117 @@ impl CPU {
             (0x8, _, _, 0x7) => self.run_8xy7(b, c),
             (0x8, _, _, 0xE) => self.run_8xyE(b, c),
             (0x9, _, _, 0x0) => self.run_9xy0(b, c),
-            (0xA, _, _, _) => self.run_Annn(b + c + d),
-            (0xB, _, _, _) => self.run_Bnnn(b + c + d),
-            (0xC, _, _, _) => self.run_Cnnn(b, c + d),
-            (0xD, _, _, _) => self.run_Dnnn(b, c, d),
-            (0xB, _, _, _) => self.run_Annn(b + c + d),
+            (0xA, _, _, _) => self.run_annn(b + c + d),
+            (0xB, _, _, _) => self.run_bnnn(b + c + d),
+            (0xC, _, _, _) => self.run_cxkk(b, c + d),
+            (0xD, _, _, _) => self.run_dxyn(b, c, d),
+            (0xE, _, 0x9, 0xE) => self.run_ex9e(b),
+            (0xE, _, 0xA, 0x1) => self.run_exa1(b),
+            (0xF, _, 0x0, 0x7) => self.run_fx07(b),
+            (0xF, _, 0x0, 0xA) => self.run_fx0a(b),
+            (0xF, _, 0x1, 0x5) => self.run_fx15(b),
+            (0xF, _, 0x1, 0x8) => self.run_fx18(b),
+            (0xF, _, 0x1, 0xE) => self.run_fx1e(b),
+            (0xF, _, 0x2, 0x9) => self.run_fx29(b),
+            (0xF, _, 0x3, 0x3) => self.run_fx33(b),
+            (0xF, _, 0x5, 0x5) => self.run_fx55(b),
+            (0xF, _, 0x6, 0x5) => self.run_fx65(b),
+            _ => 
         }
     }
+
+    // 0nnn - SYS addr
+    // Jump to a machine code routine at nnn.
+    fn run_0nnn(&self, addr: u8) -> {}
+    
+    // 00e0 - CLS
+    // Clear the display.
+    fn run_00e0(&mut self) -> () {
+        self.vram.iter_mut().for_each(|m| *m = 0);
+    }
+
+    // 1nnn - JP addr
+    // Jump to location nnn.
+    fn run_1nnn(&mut self, addr: u8) -> () {
+        self.pc = addr;
+    }
+
+    // 2nnn - CALL addr
+    // Call subroutine at nnn.
+    fn run_2nnn(&mut self, addr: u8) -> () {
+        self.sp += 1;
+        self.stack.push(self.pc);
+        self.pc = addr;
+    }
+
+    // 3xkk - SE Vx, byte
+    // Skip next instruction if Vx = kk.
+    fn run_3xkk(&mut self, x: u8, byte: u8) -> () {
+        if self.v[x] == byte {
+            self.pc += 1;
+        }
+    }
+
+    // 4xkk - SNE Vx, byte
+    // Skip next instruction if Vx != kk.
+    fn run_4xkk(&mut self, x: u8, byte: u8) -> () {
+        if self.v[x] != byte {
+            self.pc += 1;
+        }
+    }
+
+    // 5xy0 - SE Vx, Vy
+    // Skip next instruction if Vx == Vy
+    fn run_5xy0(&mut self, x: u8, y: u8) -> () {
+        if self.v[x] == self.v[y] {
+            self.pc += 1;
+        }
+    }
+
+    // 6xkk - LD Vx, byte
+    // Set Vx = Vx + kk.
+    fn run_6xkk(&mut self, x: u8, byte: u8) -> () {
+        self.v[x] = byte;
+    }
+
+    // 7xkk - ADD Vx, byte
+    // Set Vx = Vx + kk.
+    fn run_7xkk(&mut self, x: u8, byte: u8) -> () {
+        self.v[x] += byte;
+    }
+
+    // 8xy0 - LD Vx, Vy
+    // Set Vx = Vy
+    fn run_8xy0(&mut self, x: u8, y: u8) -> () {
+        self.v[x] = self.v[y];
+    }
+
+    // 8xy2 - AND Vx, Vy
+    // Set Vx = Vx AND Vy
+    fn run_8xy2(&mut self, x: u8, y: u8) -> () {
+        self.v[x] = self.v[x] & self.v[y];
+    }
+
+    // 8xy3 - XOR Vx, Vy
+    // Set Vx = Vx XOR Vy
+    fn run_8xy3(&mut self, x: u8, y: u8) -> () {
+        self.v[x] = self.v[x] ^ self.v[y];
+    }
+
+    // 8xy4 - ADD Vx, Vy
+    // Set Vx = Vx + Vy
+    fn run_8xy4(&mut self, x: u8, y: u8) -> () {
+        let result: u16 = x + y;
+        self.v[x] = result as u8;
+        self.v[0xf] = if result > 0xff { 1 } else { 0 };
+    }
+
+    // 8xy5 - SUB Vx, Vy
+    // Set Vx = Vx - Vy, set VF = NOT borrow
+    fn run_8xy5(&mut self, x: u8, y: u8) -> () {
+        self.v[0xf] = if self.v[x] > self.v[y] { 1 } else { 0 };
+        self.v[x] = self.v[x].wrapping_sub(self.v[y]);
+    }
+
+    // 
 }
